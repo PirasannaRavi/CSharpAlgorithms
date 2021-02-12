f8bc6a0e76b35f566f070d25b7655b40e3832716
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-12 12:26:38 -0500

    Added DeleteNodeLinkedList

---
+++ Problems.Tests/DeleteNodeLinkedList.Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class DeleteNodeLinkedListTests
+    {
+
+        [Test]
+        public void DeleteNodeLinkedListTest()
+        {
+            var deleteNodeLinkedList = new DeleteNodeLinkedList();
+            var node = new ListNode(5, new ListNode(1, new ListNode(9, null)));
+            deleteNodeLinkedList.DeleteNode(node);
+        }
+    }
+}
---
+++ Problems/DeleteNodeLinkedList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class DeleteNodeLinkedList
+    {
+        public void DeleteNode(ListNode node)
+        {
+            ListNode previousNode = null;
+            while (true)
+            {
+                node.val = node.next.val;
+                previousNode = node;
+                node = node.next;
+                if (node.next == null)
+                {
+                    previousNode.next = null;
+                    break;
+                }
+            }
+        }
+    }
+}
