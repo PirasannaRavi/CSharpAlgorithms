9fd93e1e0bb747944f5feb452b6787f6bceda4f9
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-12 12:02:34 -0500

    Added NumberOfStepsToReduceANumberToZero

---
+++ Problems.Tests/NumberOfStepsToReduceANumberToZero.Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class NumberOfStepsToReduceANumberToZeroTests
+    {
+
+        [Test]
+        [TestCase(14, 6)]
+        [TestCase(8, 4)]
+        [TestCase(123, 12)]
+        public void NumberOfStepsToReduceANumberToZeroTest(int num, int expected)
+        {
+            var numberOfStepsToReduceANumberToZero = new NumberOfStepsToReduceANumberToZero();
+
+            var actual = numberOfStepsToReduceANumberToZero.NumberOfSteps(num);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/NumberOfStepsToReduceANumberToZero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class NumberOfStepsToReduceANumberToZero
+    {
+        public int NumberOfSteps(int num)
+        {
+            int stepsToZero = 0;
+            while (num > 0)
+            {
+                stepsToZero++;
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                    continue;
+                }
+                num = num - 1;
+            }
+            return stepsToZero;
+        }
+    }
+}
