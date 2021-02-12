ea8c7dbde00794428b639be7692e1d28588bfe70
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-11 19:45:53 -0500

    Added MergeTwoSortedLists

---
+++ Problems.Tests/MergeTwoSortedLists.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class MergeTwoSortedListsTests
+    {
+
+        [Test]
+        public void MergeTwoSortedListsTest()
+        {
+            ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4, null)));
+            ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
+
+            var mergeTwoSortedLists = new MergeTwoSortedLists();
+
+            var actual = mergeTwoSortedLists.MergeTwoLists(l1, l2);
+            ListNode expected = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4, null))))));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/MergeTwoSortedLists.cs
@@ -0,0 +1,54 @@
+﻿namespace Problems
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class MergeTwoSortedLists
+    {
+        // Runtime: 88 ms
+        // Memory Usage: 26.3 MB
+        private ListNode tail = new ListNode(0);
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode dummyNode = tail;
+            while (true)
+            {
+                if (l1 == null)
+                {
+                    tail.next = l2;
+                    break;
+                }
+
+                if (l2 == null)
+                {
+                    tail.next = l1;
+                    break;
+                }
+
+                if (l1.val < l2.val)
+                {
+                    tail.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    tail.next = l2;
+                    l2 = l2.next;
+                }
+
+                tail = tail.next;
+            }
+
+            return dummyNode.next;
+        }
+    }
+}
