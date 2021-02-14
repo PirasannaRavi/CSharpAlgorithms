e986b0ced99e9bcd8a91a8b825f8c4bd15674ed7
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-14 00:16:47 -0500

    Added FindWordsThatCanBeFormedByCharacters

---
+++ Problems.Tests/FindWordsThatCanBeFormedByCharacters.Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class FindWordsThatCanBeFormedByCharactersTests
+    {
+
+        [Test]
+        public void FindWordsThatCanBeFormedByCharactersTest()
+        {
+            var findWordsThatCanBeFormedByCharacters = new FindWordsThatCanBeFormedByCharacters();
+
+            var actual = findWordsThatCanBeFormedByCharacters.CountCharacters(new[] { "cat", "bt", "hat", "tree" }, "atach");
+            var expected = 6;
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/FindWordsThatCanBeFormedByCharacters.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace Problems
+{
+    public class FindWordsThatCanBeFormedByCharacters
+    {
+        public int CountCharacters(string[] words, string chars)
+        {
+            int count = 0;
+            foreach (var word in words)
+            {
+                bool isWordFound = true;
+                var hashSet = ResetHash(chars);
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (hashSet.ContainsKey(word[i]))
+                    {
+                        hashSet[word[i]]--;
+                        if (hashSet[word[i]] < 0)
+                        {
+                            isWordFound = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        isWordFound = false;
+                        break;
+                    }
+                }
+
+                if (isWordFound)
+                {
+                    count += word.Length;
+                }
+
+                hashSet = null;
+            }
+
+            return count;
+        }
+        private Dictionary<char, int> ResetHash(string chars)
+        {
+            var hashSet = new Dictionary<char, int>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (hashSet.ContainsKey(chars[i]))
+                {
+                    hashSet[chars[i]]++;
+                }
+                else
+                {
+                    hashSet.Add(chars[i], 1);
+                }
+            }
+            return hashSet;
+        }
+    }
+}
