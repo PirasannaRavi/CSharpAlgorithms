63b1b9ea1f82167ef4c50c96d051224dfbbae7ed
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-15 14:58:03 -0500

    Added KWeakestRowsInAMatrix

---
+++ Problems.Tests/KWeakestRowsInAMatrix.Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class KWeakestRowsInAMatrixTests
+    {
+
+        [Test]
+        public void KWeakestRowsInAMatrixTest()
+        {
+            var kWeakestRowsInAMatrix = new KWeakestRowsInAMatrix();
+            int[][] arr = new[]{new [] {1,1,0,0,0 },
+                                new [] {1,1,1,1,0 },
+                                new [] {1,0,0,0,0 },
+                                new [] {1,1,0,0,0 },
+                                new [] {1,1,1,1,1 }};
+            int k = 3;
+            var actual = kWeakestRowsInAMatrix.KWeakestRows(arr, k);
+            var expected = new[] { 2, 0, 3 };
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/KWeakestRowsInAMatrix.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class KWeakestRowsInAMatrix
+    {
+        private int NumberOfSoldiers(int[] values)
+        {
+            var low = 0;
+            var hi = values.Length;
+
+            while (low < hi)
+            {
+                var mid = low + (hi - low) / 2;
+                if (values[mid] == 1)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    hi = mid;
+                }
+            }
+            return low;
+        }
+        public int[] KWeakestRows(int[][] mat, int k)
+        {
+            int rows = mat.Length;
+            int columns = mat[0].Length;
+            var scores = new int[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                var score = NumberOfSoldiers(mat[i]) * rows + i;
+                scores[i] = score;
+            }
+            Array.Sort(scores);
+            var returnArray = new int[k];
+            Array.Copy(scores, returnArray, k);
+
+            for (int i = 0; i < k; i++)
+            {
+                returnArray[i] = returnArray[i] % rows;
+            }
+
+            return returnArray;
+        }
+    }
+}
