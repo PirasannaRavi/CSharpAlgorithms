e4d92df42dde3552856228934623c2ce00034620
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-15 21:40:40 -0500

    Added ValidPalindrome

---
+++ Problems.Tests/ValidPalindrome.Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class ValidPalindromeTests
+    {
+
+        [Test]
+        [TestCase(".,", true)]
+        [TestCase("A man, a plan, a canal: Panama", true)]
+        [TestCase("race a car", false)]
+        public void ValidPalindromeTest(string s, bool expected)
+        {
+            var validPalindrome = new ValidPalindrome();
+
+            var actual = validPalindrome.IsPalindrome(s);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/ValidPalindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            s = s.ToLower();
+            var i = 0;
+            var j = s.Length - 1;
+            while (i < j)
+            {
+                while (i < j && !((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9')))
+                {
+                    i++;
+                }
+                while (i < j && !((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= '0' && s[j] <= '9')))
+                {
+                    j--;
+                }
+
+                if (s[i] != s[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            return true;
+        }
+    }
+}
