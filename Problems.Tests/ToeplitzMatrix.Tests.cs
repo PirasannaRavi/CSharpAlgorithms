47759c0dc79e50a9343585fb72a703c6c584f164
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-15 20:57:57 -0500

    Added ToeplitzMatrix

---
+++ Problems.Tests/ToeplitzMatrix.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class ToeplitzMatrixTests
+    {
+
+        [Test]
+        public void ToeplitzMatrixTest()
+        {
+            var toeplitzMatrix = new ToeplitzMatrix();
+            var matrix = new[]{new []{ 12, 23, -32 },
+                               new []{ -20, 12, 23 },
+                               new []{ 56, -20, 12 },
+                               new []{ 38, 56, -20 }};
+            var actual = toeplitzMatrix.IsToeplitzMatrix(matrix);
+            var expected = true;
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/ToeplitzMatrix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class ToeplitzMatrix
+    {
+        public bool IsToeplitzMatrix(int[][] matrix)
+        {
+            var rows = matrix.Length;
+            var columns = matrix[0].Length;
+            var dic = new Dictionary<int, int>();
+            for (int x = 0; x < rows; x++)
+            {
+                for (int y = 0; y < columns; y++)
+                {
+                    var key = x - y;
+                    if (dic.ContainsKey(key))
+                    {
+                        if (dic[key] != matrix[x][y])
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        dic.Add(key, matrix[x][y]);
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
