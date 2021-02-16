0903b983ad0c8955db994d62e1202a2ef7d096b0
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-16 11:53:57 -0500

    Added LetterCasePermutation

---
+++ Problems.Tests/LetterCasePermutation.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class LetterCasePermutationTests
+    {
+
+        [Test]
+        [TestCase("a1b2", new[] { "a1b2", "A1b2", "a1B2", "A1B2" })]
+        [TestCase("3z4", new[] { "3z4", "3Z4" })]
+        [TestCase("12345", new[] { "12345" })]
+        [TestCase("0", new[] { "0" })]
+        public void LetterCasePermutationTest(string s, string[] expected)
+        {
+            var letterCasePermutation = new LetterCasePermutation();
+
+            var actual = letterCasePermutation.GetLetterCasePermutation(s);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/LetterCasePermutation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class LetterCasePermutation
+    {
+        public IList<string> GetLetterCasePermutation(string S)
+        {
+            S = S.ToLower();
+            var combiStrings = new List<string>();
+            combiStrings.Add(S);
+
+            var givenChars = S.ToCharArray();
+            for (var i = 0; i < givenChars.Length; i++)
+            {
+                if (givenChars[i] >= '0' && givenChars[i] <= '9')
+                {
+                    continue;
+                }
+
+                var newCombiStrings = new List<string>();
+                for (var j = 0; j < combiStrings.Count; j++)
+                {
+                    var newString = new char[givenChars.Length];
+                    Array.Copy(combiStrings[j].ToCharArray(), newString, givenChars.Length);
+                    newString[i] = Char.ToUpper(newString[i]);
+                    newCombiStrings.Add(new string(newString));
+                }
+                combiStrings.AddRange(newCombiStrings);
+            }
+
+            return combiStrings;
+        }
+    }
+}
