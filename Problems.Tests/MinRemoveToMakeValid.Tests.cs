ee2fe46b781853e40394941524bb9da9a53f12f1
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-19 14:42:28 -0500

    Added MinimumRemoveToMakeValidParentheses

---
+++ Problems.Tests/MinRemoveToMakeValid.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class MinRemoveToMakeValidTests
+    {
+
+        [Test]
+        [TestCase("))((","")]
+        [TestCase("lee(t(c)o)de)","lee(t(c)o)de")]
+        [TestCase("a)b(c)d","ab(c)d")]
+        [TestCase("(a(b(c)d)","a(b(c)d)")]
+        public void MinRemoveToMakeValidTest(string input, string expected)
+        {
+            var minimumRemoveToMakeValidParentheses = new MinimumRemoveToMakeValidParentheses();
+
+            var actual = minimumRemoveToMakeValidParentheses.MinRemoveToMakeValid(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/MinimumRemoveToMakeValidParentheses.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class MinimumRemoveToMakeValidParentheses
+    {
+        public string MinRemoveToMakeValid(string s)
+        {
+            var openParenthesesStack = new Stack<int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    openParenthesesStack.Push(i);
+                    continue;
+                }
+                if (s[i] == ')')
+                {
+                    if (openParenthesesStack.Any())
+                    {
+                        openParenthesesStack.Pop();
+                        continue;
+                    }
+                    s = s.Remove(i, 1);
+                    i--;
+                }
+            }
+            while (openParenthesesStack.Count > 0)
+            {
+                s = s.Remove(openParenthesesStack.Pop(), 1);
+            }
+            return s;
+        }
+    }
+}
