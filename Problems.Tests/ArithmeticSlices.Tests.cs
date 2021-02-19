6a6d1be6094a63fcd531c4168363702a1a972d41
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-19 13:51:39 -0500

    Added ArithmeticSlices

---
+++ Problems.Tests/ArithmeticSlices.Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class ArithmeticSlicesTests
+    {
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 5, 6, 7 }, 2)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 6)]
+        public void ArithmeticSlicesTest(int[] input, int expected)
+        {
+            var arithmeticSlices = new ArithmeticSlices();
+
+            var actual = arithmeticSlices.NumberOfConsecutiveArithmeticSlices(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/ArithmeticSlices.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class ArithmeticSlices
+    {
+        public int NumberOfArithmeticSlicesBad(int[] A)
+        {
+            var diff = 0;
+            var numberOfArithmeticSlices = 0;
+            for (int i = 0; i < A.Length - 2; i++)
+            {
+                for (int j = i + 2; j < A.Length; j++)
+                {
+                    for (int k = i + 1; k <= j; k++)
+                    {
+                        if (k == i + 1)
+                        {
+                            diff = A[k] - A[i];
+                            continue;
+                        }
+                        if (diff != A[k] - A[k - 1])
+                        {
+                            break;
+                        }
+                        if (k == j)
+                        {
+                            numberOfArithmeticSlices++;
+                        }
+                    }
+                }
+            }
+            return numberOfArithmeticSlices;
+        }
+
+        public int NumberOfConsecutiveArithmeticSlices(int[] A)
+        {
+            // Using Dynamic Programing
+            var count = 0;
+            var result = 0;
+            for (int i = 2; i < A.Length; i++)
+            {
+                if (A[i] - A[i - 1] == A[i - 1] - A[i - 2])
+                {
+                    count++;
+                    result += count;
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            return result;
+        }
+
+        public int NumberOfSubsequenceArithmeticSlices(int[] A)
+        {
+            var count = 0;
+            var result = 0;
+            for (int i = 2; i < A.Length; i++)
+            {
+                if (A[i] - A[i - 1] == A[i - 1] - A[i - 2])
+                {
+                    count++;
+                    result += count;
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            return result;
+        }
+    }
+}
