f3063e2cb0400e8c00fed1de12a456ba9601e266
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-22 10:25:53 -0500

    Added BrokenCalculator

---
+++ Problems.Tests/BrokenCalculator.Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class BrokenCalculatorTests
+    {
+
+        [Test]
+        [TestCase(1024, 1, 1023)]
+        [TestCase(5, 8, 2)]
+        [TestCase(3, 10, 3)]
+        [TestCase(2, 9, 6)]
+        [TestCase(2, 3, 2)]
+        public void BrokenCalculatorTest(int X, int Y, int expected)
+        {
+            var brokenCalculator = new BrokenCalculator();
+
+            var actual = brokenCalculator.BrokenCalc(X, Y);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/BrokenCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class BrokenCalculator
+    {
+        public int BrokenCalc(int X, int Y)
+        {
+            // 02 -> 9
+            // 01. 02 x 2
+            // 02. 04 - 1
+            // 03. 03 x 2
+            // 04. 06 - 1
+            // 05. 05 x 2
+            // 06. 10 - 1
+            int numOfOperations = 0;
+            while (Y > X)
+            {
+                numOfOperations++;
+                if (Y % 2 == 1)
+                    Y++;
+                else
+                    Y /= 2;
+            }
+
+            return numOfOperations + X - Y;
+        }
+    }
+}
