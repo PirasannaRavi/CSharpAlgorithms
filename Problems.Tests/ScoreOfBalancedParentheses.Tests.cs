6443981a37ab1a80f22777014755762f9fe9bb9f
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-24 13:56:15 -0500

    Added ScoreOfBalancedParentheses

---
+++ Problems.Tests/ScoreOfBalancedParentheses.Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class ScoreOfBalancedParenthesesTests
+    {
+
+        [Test]
+        [TestCase("()(()((())))", 11)]
+        [TestCase("()()", 2)]
+        [TestCase("(())", 2)]
+        [TestCase("()", 1)]
+        [TestCase("(()(()))", 6)]
+        public void ScoreOfBalancedParenthesesTest(string S, int expected)
+        {
+            var scoreOfBalancedParentheses = new ScoreOfBalancedParentheses();
+
+            var actual = scoreOfBalancedParentheses.ScoreOfParentheses(S);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/ScoreOfBalancedParentheses.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class ScoreOfBalancedParentheses
+    {
+        public int ScoreOfParentheses(string S)
+        {
+            var balancedStack = new Stack<int>();
+            balancedStack.Push(0);
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == '(')
+                {
+                    balancedStack.Push(0);
+                }
+                else if (S[i] == ')')
+                {
+                    var ultimateValue = balancedStack.Pop();
+                    var penultimateValue = balancedStack.Pop();
+                    balancedStack.Push(penultimateValue + Math.Max(ultimateValue * 2, 1));
+                }
+            }
+            return balancedStack.Pop();
+        }
+    }
+}
