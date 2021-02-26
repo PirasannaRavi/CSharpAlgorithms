c58de67b2a2c3299ba178ecffdd8fe1be2991d8c
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-26 11:54:03 -0500

    Added ShortestUnsortedContinuousSubarray

---
+++ Problems.Tests/ShortestUnsortedContinuousSubarray.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class ShortestUnsortedContinuousSubarrayTests
+    {
+
+        [Test]
+        [TestCase(new[] { 2, 1 }, 2)]
+        [TestCase(new[] { 1 }, 0)]
+        [TestCase(new[] { 1, 2, 3, 4 }, 0)]
+        [TestCase(new[] { 2, 6, 4, 8, 10, 9, 15 }, 5)]
+        public void ShortestUnsortedContinuousSubarrayTest(int[] nums, int expected)
+        {
+            var shortestUnsortedContinuousSubarray = new ShortestUnsortedContinuousSubarray();
+
+            var actual = shortestUnsortedContinuousSubarray.FindUnsortedSubarray(nums);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/ShortestUnsortedContinuousSubarray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class ShortestUnsortedContinuousSubarray
+    {
+        public int FindUnsortedSubarray(int[] nums)
+        {
+            int len = nums.Length - 1, left = -1, right = -1, max = nums[0], min = nums[len];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int a = nums[i], b = nums[len - i];
+
+                if (a < max)
+                    right = i;
+                else
+                    max = a;
+
+                if (b > min)
+                    left = i;
+                else
+                    min = b;
+            }
+            var returnVal = Math.Max(0, left + right - len + 1);
+            return returnVal;
+        }
+    }
+}
