47277e33e7fca282956b05b56c015faa463b04f4
Pirasanna Ravi <pirasanna.ravi@live.com>
2021-02-26 13:40:29 -0500

    Added ValidateStackSequencesAlgo

---
+++ Problems.Tests/ValidateStackSequencesAlgo.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Problems;
+
+namespace Problems.Tests
+{
+    [TestFixture]
+    public class ValidateStackSequencesAlgoTests
+    {
+        [Test]
+        [TestCase(new int[0], new int[0], true)]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4, 5 }, true)]
+        [TestCase(new[] { 0, 2, 1 }, new[] { 0, 1, 2 }, true)]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { 4, 3, 5, 1, 2 }, false)]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { 4, 5, 3, 2, 1 }, true)]
+        public void ValidateStackSequencesAlgoTest(int[] pushed, int[] popped, bool expected)
+        {
+            var validateStackSequencesAlgo = new ValidateStackSequencesAlgo();
+
+            var actual = validateStackSequencesAlgo.validateStackSequences(pushed, popped);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Problems/ValidateStackSequencesAlgo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class ValidateStackSequencesAlgo
+    {
+        public bool ValidateStackSequences1(int[] pushed, int[] popped)
+        {
+            if (pushed.Length == 0 && popped.Length == 0)
+            {
+                return true;
+            }
+            var tryStack = new Stack<int>();
+            tryStack.Push(pushed[0]);
+            var j = 1;
+            for (int i = 0; i < popped.Length; i++)
+            {
+                if (tryStack.Any())
+                {
+                    if (tryStack.Peek() == popped[i])
+                    {
+                        tryStack.Pop();
+                        continue;
+                    }
+                }
+                if (j >= pushed.Length)
+                {
+                    return false;
+                }
+                while (j < pushed.Length && popped[i] != pushed[j])
+                {
+                    tryStack.Push(pushed[j]);
+                    j++;
+                }
+                j++;
+            }
+
+            return true;
+        }
+
+        public bool validateStackSequences(int[] pushed, int[] popped)
+        {
+            int N = pushed.Length;
+            var stack = new Stack<int>();
+
+            int j = 0;
+            foreach (var x in pushed)
+            {
+                stack.Push(x);
+                while (stack.Any() && j < N && stack.Peek() == popped[j])
+                {
+                    stack.Pop();
+                    j++;
+                }
+            }
+
+            return j == N;
+        }
+    }
+}
+
